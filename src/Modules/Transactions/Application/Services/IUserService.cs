5b2e66c63c2a9d29d79910811abf71b193c43f06
Lukasz Swiderski <lukasz@swiderski.xyz>
2021-10-31 16:00:04 +0100

    Working on user seed service

---
+++ src/Modules/Transactions/Application/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BudgetUnderControl.Modules.Transactions.Application.Services
+{
+    public interface IUserService
+    {
+        Task SeedNewUserAsync(Guid userId);
+    }
+}
---
+++ src/Modules/Transactions/Infrastructure/Services/UserSerivce/UserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using BudgetUnderControl.Modules.Transactions.Application.Services;
+
+namespace BudgetUnderControl.Modules.Transactions.Infrastructure.Services.UserSerivce
+{
+    public class UserService : IUserService
+    {
+        private readonly ICategoryService categoryService;
+
+        public UserService(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public async Task SeedNewUserAsync(Guid userId)
+        {
+            await this.categoryService.SeedCategoriesAsync(userId);
+        }
+    }
+}
